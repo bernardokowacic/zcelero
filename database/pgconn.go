fc89f36442d7e8704dfd9367796af5748bea5ca4
bernardokowacic <berkowacic@gmail.com>
2022-11-03 18:53:14 -0300

    fix: change database name

--- database/pgconn.go
+++ database/pgconn.go
@@ -10,7 +10,7 @@ import (
 
 // CreatePGConn opens the database connection
 func CreatePGConn() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=database user=postgres password=%s dbname=posterr port=5432 sslmode=disable TimeZone=America/Sao_Paulo", os.Getenv("POSTGRES_PASSWORD"))
+	dsn := fmt.Sprintf("host=database user=postgres password=%s dbname=zcelero port=5432 sslmode=disable TimeZone=America/Sao_Paulo", os.Getenv("POSTGRES_PASSWORD"))
 	db, err := gorm.Open(postgres.New(postgres.Config{DSN: dsn}), &gorm.Config{DisableForeignKeyConstraintWhenMigrating: true})
 	if err != nil {
 		return nil, err
