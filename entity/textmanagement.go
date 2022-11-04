58084c73d33434cd05e87496cf9cd3e7669de521
bernardokowacic <berkowacic@gmail.com>
2022-11-04 19:03:22 -0300

    refactor: remove unused value from struct

--- entity/textmanagement.go
+++ entity/textmanagement.go
@@ -1,7 +1,6 @@
 package entity
 
 type TextManagement struct {
-	ID                 uint64 `json:"id"`
 	TextData           string `json:"text_data" binding:"required"`
 	Encryption         *bool  `json:"encryption" binding:"required"`
 	KeySize            uint64 `json:"key_size" binding:"oneof=1024 2048 4096"`
