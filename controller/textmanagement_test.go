75dc89ff6db0191b5e8fc0b1d0538bd156e4c7ab
bernardokowacic <berkowacic@gmail.com>
2022-11-04 18:56:50 -0300

    feat: add unit test to get end-point

---
+++ controller/textmanagement_test.go
@@ -0,0 +1,102 @@
+package controller_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"zcelero/helper"
+	serviceMock "zcelero/mocks/service"
+
+	"github.com/go-playground/assert/v2"
+)
+
+func TestGetUserRoute(t *testing.T) {
+	service := &serviceMock.TextManagementServiceInteface{}
+	router := helper.StartAPI(service)
+
+	uuid := "154ad8a0-1e42-4cf6-9d7b-e49f71dcc4ec"
+	args := struct {
+		PrivateKey         string `json:"private_key"`
+		PrivateKeyPassword string `json:"private_key_password"`
+	}{
+		PrivateKey:         "private_key",
+		PrivateKeyPassword: "aaa",
+	}
+	body, _ := json.Marshal(args)
+
+	service.On("Get", uuid, args.PrivateKey, args.PrivateKeyPassword).Return("message", nil)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/v1/text-management?text_id="+uuid, bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestGetUserRouteWithoutTextID(t *testing.T) {
+	service := &serviceMock.TextManagementServiceInteface{}
+	router := helper.StartAPI(service)
+
+	args := struct {
+		PrivateKey         string `json:"private_key"`
+		PrivateKeyPassword string `json:"private_key_password"`
+	}{
+		PrivateKey:         "private_key",
+		PrivateKeyPassword: "aaa",
+	}
+	body, _ := json.Marshal(args)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/v1/text-management", bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotAcceptable, w.Code)
+}
+
+func TestGetUserRouteWithServiceError(t *testing.T) {
+	service := &serviceMock.TextManagementServiceInteface{}
+	router := helper.StartAPI(service)
+
+	uuid := "154ad8a0-1e42-4cf6-9d7b-e49f71dcc4ec"
+	args := struct {
+		PrivateKey         string `json:"private_key"`
+		PrivateKeyPassword string `json:"private_key_password"`
+	}{
+		PrivateKey:         "private_key",
+		PrivateKeyPassword: "aaa",
+	}
+	body, _ := json.Marshal(args)
+
+	service.On("Get", uuid, args.PrivateKey, args.PrivateKeyPassword).Return("", errors.New("some error"))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/v1/text-management?text_id="+uuid, bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestGetUserRouteBidingError(t *testing.T) {
+	service := &serviceMock.TextManagementServiceInteface{}
+	router := helper.StartAPI(service)
+
+	uuid := "154ad8a0-1e42-4cf6-9d7b-e49f71dcc4ec"
+	args := struct {
+		PrivateKey         string `json:"private_key"`
+		PrivateKeyPassword int    `json:"private_key_password"`
+	}{
+		PrivateKey:         "private_key",
+		PrivateKeyPassword: 12,
+	}
+	body, _ := json.Marshal(args)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/v1/text-management?text_id="+uuid, bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotAcceptable, w.Code)
+}
