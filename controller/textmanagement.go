078beefd2a3bf957d3f4987fe1078e1228447e23
bernardokowacic <berkowacic@gmail.com>
2022-11-03 22:57:41 -0300

    feat: implement insert handler

--- controller/textmanagement.go
+++ controller/textmanagement.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"zcelero/database/entity"
 	"zcelero/service"
 
 	"github.com/gin-gonic/gin"
@@ -15,6 +16,23 @@ func Get(textManagementService service.TextManagementServiceInteface) gin.Handle
 
 func Insert(textManagementService service.TextManagementServiceInteface) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+		var json entity.TextManagement
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if *json.Encryption && json.PrivateKeyPassword == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "private_key_password is required when encryption is true"})
+			return
+		}
+
+		response, err := textManagementService.Insert(json)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"uuid": response.Uuid, "private_key": response.PrivateKey})
 	}
 }
