6c2f7fa3fdc1afdd58e338a597f0fa8447c8e47b
bernardokowacic <berkowacic@gmail.com>
2022-11-05 14:26:49 -0300

    feat: add unit test to post end-point

--- controller/textmanagement_test.go
+++ controller/textmanagement_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"zcelero/entity"
 	"zcelero/helper"
 	serviceMock "zcelero/mocks/service"
 
@@ -99,4 +100,141 @@ func TestGetUserRouteBidingError(t *testing.T) {
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusNotAcceptable, w.Code)
+}
+
+func TestPostUserRouteWithoutEncryptation(t *testing.T) {
+	service := &serviceMock.TextManagementServiceInteface{}
+	router := helper.StartAPI(service)
+
+	encryptation := false
+	args := entity.TextManagement{
+		TextData:   "text data",
+		Encryption: &encryptation,
+	}
+	body, _ := json.Marshal(args)
+
+	response := entity.TextManagement{
+		TextData:   args.TextData,
+		Encryption: args.Encryption,
+		Uuid:       "uuid",
+	}
+
+	service.On("Insert", args).Return(response, nil)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/v1/text-management", bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestPostUserRouteWithEncryptation(t *testing.T) {
+	service := &serviceMock.TextManagementServiceInteface{}
+	router := helper.StartAPI(service)
+
+	encryptation := true
+	args := entity.TextManagement{
+		TextData:           "text data",
+		Encryption:         &encryptation,
+		KeySize:            1024,
+		PrivateKeyPassword: "aaa",
+	}
+	body, _ := json.Marshal(args)
+
+	response := entity.TextManagement{
+		TextData:           args.TextData,
+		Encryption:         args.Encryption,
+		KeySize:            args.KeySize,
+		PrivateKeyPassword: args.PrivateKeyPassword,
+		Uuid:               "uuid",
+		PrivateKey:         "private_key",
+	}
+
+	service.On("Insert", args).Return(response, nil)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/v1/text-management", bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestPostUserRouteWithBindingError(t *testing.T) {
+	service := &serviceMock.TextManagementServiceInteface{}
+	router := helper.StartAPI(service)
+
+	encryptation := true
+	args := entity.TextManagement{
+		Encryption:         &encryptation,
+		KeySize:            1024,
+		PrivateKeyPassword: "aaa",
+	}
+	body, _ := json.Marshal(args)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/v1/text-management", bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestPostUserRouteWithoutPassword(t *testing.T) {
+	service := &serviceMock.TextManagementServiceInteface{}
+	router := helper.StartAPI(service)
+
+	encryptation := true
+	args := entity.TextManagement{
+		TextData:   "text data",
+		Encryption: &encryptation,
+		KeySize:    1024,
+	}
+	body, _ := json.Marshal(args)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/v1/text-management", bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotAcceptable, w.Code)
+}
+
+func TestPostUserRouteWithWrongKeySize(t *testing.T) {
+	service := &serviceMock.TextManagementServiceInteface{}
+	router := helper.StartAPI(service)
+
+	encryptation := true
+	args := entity.TextManagement{
+		TextData:           "text data",
+		Encryption:         &encryptation,
+		PrivateKeyPassword: "aaa",
+		KeySize:            12,
+	}
+	body, _ := json.Marshal(args)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/v1/text-management", bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotAcceptable, w.Code)
+}
+
+func TestPostUserRouteWithInsertError(t *testing.T) {
+	service := &serviceMock.TextManagementServiceInteface{}
+	router := helper.StartAPI(service)
+
+	encryptation := true
+	args := entity.TextManagement{
+		TextData:           "text data",
+		Encryption:         &encryptation,
+		KeySize:            1024,
+		PrivateKeyPassword: "aaa",
+	}
+	body, _ := json.Marshal(args)
+
+	service.On("Insert", args).Return(entity.TextManagement{}, errors.New("error"))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/v1/text-management", bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
 }
